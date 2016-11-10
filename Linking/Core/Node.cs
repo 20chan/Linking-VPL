d1a2ec8546d1e7913347d52ffaf1832b51c2ce04
philly <sonagi.philly@gmail.com>
2016-11-10 12:00:23 +0900

    Implement NodeCollection

--- Linking/Core/Node.cs
+++ Linking/Core/Node.cs
@@ -12,150 +12,183 @@ namespace Linking.Core
         {
             Parent = parent;
         }
-    }
 
-    public class NodeCollection : IList, ICollection, IEnumerable, ICloneable
-    {
-        private Node _owner;
-        public Node Owner => _owner;
-
-        private ArrayList _list;
-
-        public int Count => _list.Count;
-
-        public NodeCollection(Node owner)
+        protected virtual void OnNodeAdded()
         {
-            _owner = owner;
-            this._list = new ArrayList();
+
         }
 
-        public void Add(Node value)
+
+        public class NodeCollection : IList, ICollection, IEnumerable, ICloneable
         {
-            throw new NotImplementedException();
-        }
+            private Node _owner;
+            public Node Owner => _owner;
 
-        public void AddRange(Node[] nodes)
-        {
-            throw new NotImplementedException();
-        }
+            private ArrayList _list;
 
-        public void Clear()
-        {
-            throw new NotImplementedException();
-        }
+            public int Count => _list.Count;
 
-        public bool Contains(Node node) => _list.Contains(node);
-
-        public void CopyTo(Array array, int index)
-        {
-            this._list.CopyTo(array, index);
-        }
-
-        public IEnumerator GetEnumerator() => new NodeCollectionEnumerator(this);
-
-        public int IndexOf(Node node) => _list.IndexOf(node);
-
-        public void Remove(Node node)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void RemoveAt(int index) => this.Remove(this[index]);
-
-        int IList.Add(object value)
-        {
-            throw new NotImplementedException();
-        }
-
-        void IList.Remove(object value)
-        {
-            throw new NotImplementedException();
-        }
-
-        void IList.RemoveAt(int index)
-        {
-            throw new NotImplementedException();
-        }
-
-        void IList.Clear() => _list.Clear();
-
-        bool IList.Contains(object value) => _list.Contains(value);
-
-        int IList.IndexOf(object value) => _list.IndexOf(value);
-
-        void IList.Insert(int index, object value)
-        {
-            throw new NotSupportedException();
-        }
-
-        bool IList.IsReadOnly => _list.IsReadOnly;
-
-        bool IList.IsFixedSize => _list.IsFixedSize;
-
-        object IList.this[int index]
-        {
-            get
+            public NodeCollection(Node owner)
             {
-                return _list[index];
+                _owner = owner;
+                this._list = new ArrayList();
             }
-            set
+
+            public void Add(Node value)
+            {
+                _list.Add(value);
+                _owner.OnNodeAdded();
+            }
+
+            public void AddRange(Node[] nodes)
+            {
+                if (nodes == null)
+                    throw new ArgumentNullException("nodes");
+                if (nodes.Length != 0)
+                {
+                    for (int i = 0; i < nodes.Length; i++)
+                        this.Add(nodes[i]);
+                }
+            }
+
+            public void Clear()
+            {
+                while (Count != 0)
+                    RemoveAt(Count - 1);
+            }
+
+            public bool Contains(Node node) => _list.Contains(node);
+
+            public void CopyTo(Array array, int index)
+            {
+                this._list.CopyTo(array, index);
+            }
+
+            public IEnumerator GetEnumerator() => new NodeCollectionEnumerator(this);
+
+            public int IndexOf(Node node) => _list.IndexOf(node);
+
+            public void Remove(Node node)
+            {
+                if (node != null)
+                {
+                    _list.Remove(node);
+                }
+            }
+
+            public void RemoveAt(int index) => this.Remove(this[index]);
+
+            #region IList
+
+            int IList.Add(object value)
+            {
+                if (!(value is Node))
+                    throw new ArgumentException("NodeBadNode", "node");
+                this.Add((Node)value);
+                return this.IndexOf((Node)value);
+            }
+
+            void IList.Remove(object value)
+            {
+                if (value is Node)
+                {
+                    this.Remove((Node)value);
+                }
+            }
+
+            void IList.RemoveAt(int index)
+            {
+                this.RemoveAt(index);
+            }
+
+            void IList.Clear() => _list.Clear();
+
+            bool IList.Contains(object value) => _list.Contains(value);
+
+            int IList.IndexOf(object value) => _list.IndexOf(value);
+
+            void IList.Insert(int index, object value)
             {
                 throw new NotSupportedException();
             }
-        }
 
-        bool ICollection.IsSynchronized => _list.IsSynchronized;
+            bool IList.IsReadOnly => _list.IsReadOnly;
 
-        object ICollection.SyncRoot => _list.SyncRoot;
+            bool IList.IsFixedSize => _list.IsFixedSize;
 
-        object ICloneable.Clone()
-        {
-            throw new NotImplementedException();
-        }
-
-        public Node this[int index]
-        {
-            get
-            {
-                return (Node)_list[index];
-            }
-        }
-
-        private class NodeCollectionEnumerator : IEnumerator
-        {
-            private NodeCollection _nodes;
-            private int current;
-            private int originalCount;
-
-            public NodeCollectionEnumerator(NodeCollection nodes)
-            {
-                this._nodes = nodes;
-                this.originalCount = nodes.Count;
-                this.current = -1;
-            }
-
-            public bool MoveNext()
-            {
-                if(current < (_nodes.Count - 1) && current < (originalCount - 1))
-                {
-                    current++;
-                    return true;
-                }
-                return false;
-            }
-
-            public void Reset()
-            {
-                this.current = -1;
-            }
-
-            public object Current
+            object IList.this[int index]
             {
                 get
                 {
-                    if (this.current == -1)
-                        return null;
-                    return this._nodes[this.current];
+                    return _list[index];
+                }
+                set
+                {
+                    throw new NotSupportedException();
+                }
+            }
+
+            #endregion
+            #region ICollection
+
+            bool ICollection.IsSynchronized => _list.IsSynchronized;
+
+            object ICollection.SyncRoot => _list.SyncRoot;
+
+            #endregion
+            #region ICloneable
+
+            object ICloneable.Clone()
+            {
+                throw new NotImplementedException();
+            }
+
+            #endregion
+
+            public Node this[int index]
+            {
+                get
+                {
+                    return (Node)_list[index];
+                }
+            }
+
+            private class NodeCollectionEnumerator : IEnumerator
+            {
+                private NodeCollection _nodes;
+                private int current;
+                private int originalCount;
+
+                public NodeCollectionEnumerator(NodeCollection nodes)
+                {
+                    this._nodes = nodes;
+                    this.originalCount = nodes.Count;
+                    this.current = -1;
+                }
+
+                public bool MoveNext()
+                {
+                    if (current < (_nodes.Count - 1) && current < (originalCount - 1))
+                    {
+                        current++;
+                        return true;
+                    }
+                    return false;
+                }
+
+                public void Reset()
+                {
+                    this.current = -1;
+                }
+
+                public object Current
+                {
+                    get
+                    {
+                        if (this.current == -1)
+                            return null;
+                        return this._nodes[this.current];
+                    }
                 }
             }
         }
