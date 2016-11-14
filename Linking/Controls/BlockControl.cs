6700da8b554e4bcf047ce7cebb94226d66dd131c
philly <sonagi.philly@gmail.com>
2016-11-14 21:37:49 +0900

    Add BlockControl usercontrol.

---
+++ Linking/Controls/BlockControl.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Linking.Controls
+{
+    public partial class BlockControl : UserControl
+    {
+        public BlockControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
