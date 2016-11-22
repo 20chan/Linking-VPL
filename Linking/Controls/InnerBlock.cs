93e4b990d8ac990560775fc42b5deafbb4f7ddc0
philly <sonagi.philly@gmail.com>
2016-11-22 20:26:09 +0900

    Add InnerBlock class

---
+++ Linking/Controls/InnerBlock.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Linking.Controls
+{
+    public partial class InnerBlock : UserControl
+    {
+        public InnerBlock()
+        {
+            InitializeComponent();
+        }
+
+        public void SetBlock(BlockControl block)
+        {
+            //괜찮아 다 잘될거야!
+            //난 널 믿어 미래의 나!
+            throw new NotImplementedException();
+        }
+    }
+}
